f17c28c218d1c27c8f77db953d7bde62473f3bb4
Rhea <rheaKrishnan30@gmail.com>
2025-11-09 13:22:36 -0600

    Robot centric drive - working version

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotCentricDrive.java
@@ -1,13 +1,30 @@
 package org.firstinspires.ftc.teamcode;
+import static org.firstinspires.ftc.teamcode.Configuration.MAX_MOTOR_SPEED;
+import static org.firstinspires.ftc.teamcode.Configuration.MOTOR_STOP;
+import static org.firstinspires.ftc.teamcode.Configuration.SERVO_MOVE_TIME;
+import static org.firstinspires.ftc.teamcode.Configuration.SLIDE_MAX_HEIGHT;
+import static org.firstinspires.ftc.teamcode.Configuration.SLIDE_MIN_HEIGHT;
+import static org.firstinspires.ftc.teamcode.Configuration.SLIDE_TIMEOUT;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 @TeleOp(name="Robot Centric Drive", group="Drive")
 public class RobotCentricDrive extends LinearOpMode {
 
     public HardwareDrive robot = new HardwareDrive();
     //@Override
+    private boolean aPressed = false;
+    private boolean trayMoving = false;
+     // Debounce flags
+    private boolean xPressed = false;
+    private boolean bPressed = false;
+
+
     public void runOpMode() {
         robot.init(hardwareMap);
+        ElapsedTime timer = new ElapsedTime();
         telemetry.addLine("Ready for start");
         telemetry.update();
 
@@ -31,15 +48,53 @@ public class RobotCentricDrive extends LinearOpMode {
             max = Math.max(max, Math.abs(frontRightPower));
             max = Math.max(max, Math.abs(backRightPower));
 
-           //Move motor based on the power needed
+            //Move motor based on the power needed
             robot.moveMotor(robot.frontLeft, frontLeftPower / max,
                     robot.frontRight, frontRightPower / max,
                     robot.backLeft, backLeftPower / max,
                     robot.backRight, backRightPower / max
-                    );
+            );
+//Intake
+            if (gamepad1.right_trigger>0.05) { // intake motor brings balls in
+                robot.intakeMotor.setPower(MAX_MOTOR_SPEED);
+            }
+            else
+            {
+                robot.intakeMotor.setPower(MOTOR_STOP);
+            }
+            if (gamepad1.left_trigger>0.05) { // push balls out, just in case
+                robot.intakeMotor.setPower(-MAX_MOTOR_SPEED);
+            }
+            else
+            {
+                robot.intakeMotor.setPower(MOTOR_STOP);
+            }
+
+// Slides
+            if (gamepad1.right_bumper) {
+                robot.moveSlidesToPosition(SLIDE_MAX_HEIGHT, SLIDE_TIMEOUT);
+            }
+            if (gamepad1.left_bumper) {
+                robot.moveSlidesToPosition(SLIDE_MIN_HEIGHT, SLIDE_TIMEOUT);
+            }
+
+//SERVO MOVEMENT
+// Next position (X)
+            if (gamepad2.x && !xPressed) {
+                xPressed = true;
+                robot.nextTrayPosition();
+            }
+            if (!gamepad2.x) xPressed = false;
+
+            // Previous position (B)
+            if (gamepad2.b && !bPressed) {
+                bPressed = true;
+                robot.prevTrayPosition();
+            }
+            if (!gamepad2.b) bPressed = false;
 
 
 
         }
-    }
+}
 }
