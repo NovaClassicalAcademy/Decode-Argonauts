d5bab95786dcf78339a56982c653ed6f380cc587
Rhea <rheaKrishnan30@gmail.com>
2025-11-09 13:07:26 -0600

    Configuration file with global constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Configuration.java
@@ -3,41 +3,51 @@ package org.firstinspires.ftc.teamcode;
 public class Configuration {
 
     // Motor ports - Drivetrain - REV Hub
-    public static final int FRONT_LEFT_MOTOR_PORT = 3;//3
-    public static final int BACK_LEFT_MOTOR_PORT = 2;//1
-    public static final int FRONT_RIGHT_MOTOR_PORT = 1;//0
-    public static final int BACK_RIGHT_MOTOR_PORT = 0;//3
+    public static final int FRONT_LEFT_MOTOR_PORT = 3;
+    public static final int BACK_LEFT_MOTOR_PORT = 2;
+    public static final int FRONT_RIGHT_MOTOR_PORT = 1;
+    public static final int BACK_RIGHT_MOTOR_PORT = 0;
 
+    //intake Motor port
+    public static final int INTAKE_MOTOR_PORT = 0;
 
     //Motor port - Servo
     public static final int SERVO_MOTOR_PORT = 0;
 
     //Motor Name - Servo
-    public static final String SERVO_MOTOR = "servoMotor";//3
+    public static final String SERVO_MOTOR_TRAY = "servoMotorTray";//3
+
+    //Servo positions
+    public static final double SERVO_TRAY_MIN = 0.2;
+    public static final double SERVO_MOVE_TIME = 0.4;
+    public static final double SERVO_TRAY_SLOT1 = 0.0;
+    public static final double SERVO_TRAY_SLOT2 = 0.5;
+    public static final double SERVO_TRAY_SLOT3 = 1.0;
+
     // Motor port - Intake
-    public static final int INTAKE_MOTOR_PORT = 2;
 
     // Motor Names - Drivetrain
-    public static final String FRONT_LEFT_MOTOR = "leftFront";//3
-    public static final String BACK_LEFT_MOTOR = "leftRear";//2
-    public static final String FRONT_RIGHT_MOTOR = "rightFront";//1
-    public static final String BACK_RIGHT_MOTOR = "rightRear";//0
+    public static final String FRONT_LEFT_MOTOR = "frontLeftMotor";//3
+    public static final String BACK_LEFT_MOTOR = "backLeftMotor";//2
+    public static final String FRONT_RIGHT_MOTOR = "frontRightMotor";//1
+    public static final String BACK_RIGHT_MOTOR = "backRightMotor";//0
 
-    // Motor Name - Park Slider
+    //intake Motor
+    public static final String INTAKE_MOTOR = "intakeMotor";//
+
+    // Motor Name - Slider
     public static final String SLIDER_MOTOR_LEFT = "sliderMotorLeft";
     public static final String SLIDER_MOTOR_RIGHT = "sliderMotorRight";
 
-
-
     // Motor speeds (Power settings)
-    public static final double MAX_MOTOR_SPEED = 0.8;
+    public static final double MAX_MOTOR_SPEED = 1.0;
     public static final double MIN_MOTOR_SPEED = 0.2;
     public static final double MOTOR_STOP = 0.0;
 
 
     // Slider Motor speeds (Power settings)
     public static final double SLIDER_SPEED = 0.8;
-    public static final double SLIDER_SPEED_HANG = 1.0;
+    public static final double SLIDER_HOLD_SPEED = 0.1;
 
     // Robot Movement Speeds (Drive system)
     public static final double DRIVE_SPEED = 0.5; // 50% speed
@@ -63,5 +73,7 @@ public class Configuration {
     public static final String VAL_21 = "";
     public static final String VAL_22 = "";
     public static final String VAL_23 = "";
+
+
 
 }
