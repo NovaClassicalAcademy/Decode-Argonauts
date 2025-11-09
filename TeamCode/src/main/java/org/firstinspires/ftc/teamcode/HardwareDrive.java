cec3d59552f3ca6f47b7409e12871605888d5a64
Rhea <rheaKrishnan30@gmail.com>
2025-11-08 23:53:43 -0600

    Added function override.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareDrive.java
@@ -1,9 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.teamcode.Configuration.BACK_LEFT_MOTOR;
+import static org.firstinspires.ftc.teamcode.Configuration.BACK_RIGHT_MOTOR;
+import static org.firstinspires.ftc.teamcode.Configuration.FRONT_LEFT_MOTOR;
+import static org.firstinspires.ftc.teamcode.Configuration.FRONT_RIGHT_MOTOR;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
 //Declare the motors for the drivetrain,map the hardware and functions for the drivetrain
 public class HardwareDrive {
+
     // Declare Motors
     public DcMotor frontLeft = null;
     public DcMotor frontRight = null;
@@ -12,11 +19,12 @@ public class HardwareDrive {
 
     // Initialize hardware from the configuration
     public void init(HardwareMap hwMap) {
+
         // Map motors from Driver Station config names
-        frontLeft = hwMap.get(DcMotor.class, "frontLeftMotor");
-        frontRight = hwMap.get(DcMotor.class, "frontRightMotor");
-        backLeft = hwMap.get(DcMotor.class, "backLeftMotor");
-        backRight = hwMap.get(DcMotor.class, "backRightMotor");
+        frontLeft = hwMap.get(DcMotor.class,FRONT_LEFT_MOTOR);
+        frontRight = hwMap.get(DcMotor.class, FRONT_RIGHT_MOTOR);
+        backLeft = hwMap.get(DcMotor.class, BACK_LEFT_MOTOR);
+        backRight = hwMap.get(DcMotor.class, BACK_RIGHT_MOTOR);
 
         // Set motor directions
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -30,15 +38,25 @@ public class HardwareDrive {
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
-    public void moveMotor(double frontLeftPower,
-                          double frontRightPower,
-                          double backLeftPower,
-                          double backRightPower)
+    public void moveMotor(DcMotor frontLeft, double frontLeftPower,
+                          DcMotor frontRight, double frontRightPower,
+                          DcMotor backLeft, double backLeftPower,
+                          DcMotor backRight, double backRightPower)
         {
             frontLeft.setPower(frontLeftPower);
             frontRight.setPower(frontRightPower);
             backLeft.setPower(backLeftPower);
             backRight.setPower(backRightPower);
         }
+    public void moveMotor(DcMotor frontLeft,
+                          DcMotor frontRight,
+                          DcMotor backLeft,
+                          DcMotor backRight, double motorPower)
+    {
+        frontLeft.setPower(motorPower);
+        frontRight.setPower(motorPower);
+        backLeft.setPower(motorPower);
+        backRight.setPower(motorPower);
+    }
 
 }
